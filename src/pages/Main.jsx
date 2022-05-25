ece6104ee945dcbf0dab255e64a04e167ca96361
hnrqblck <hnrqblck@gmail.com>
2022-05-24 21:28:08 -0300

    [JS-206] feat: add json and csv button & organize JSON and csv file

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,5 +1,6 @@
-import { Box, ListItem, UnorderedList, Heading } from '@chakra-ui/react';
+import { Box, ListItem, UnorderedList, Heading, Button } from '@chakra-ui/react';
 import { useEffect, useState } from 'react';
+import { CSVLink } from 'react-csv';
 import * as api from 'strateegia-api';
 import MapList from '../components/MapList';
 import ProjectList from '../components/ProjectList';
@@ -12,6 +13,7 @@ export default function Main() {
   const [selectedMap, setSelectedMap] = useState('');
   const [isLoading, setIsLoading] = useState(false);
   const [convergencePoints, setConvergencePoints] = useState([]);
+  
   const [accessToken, setAccessToken] = useState('');
   const [isCreate, setIsCreate] = useState(false);
   const [textForCreate, setTextForCreate] = useState(initialTextForCreate);
@@ -93,26 +95,86 @@ export default function Main() {
 }
 
 function ConvergencePointList({ convergencePoints }) {
+  // console.log('convergencePoints', convergencePoints)
+  const [csv, setCsv] = useState([]);
+
+  useEffect(() => {
+    setCsv([])
+    convergencePoints.map(point => point.questions.map(({text, options}) => {
+      setCsv(csv => [...csv, {text: text, option: options.map(({text}) => text), results: options.map(({average}) => (average * 100).toFixed(2) + '%')} ]);
+    }));
+    
+  }, [convergencePoints])
+
+  // useEffect(() => {
+    console.log('csv' ,csv)
+  // }, [csv]);
+  // console.log('cgPoint', cgPoint)
   return (
     <Box>
       {convergencePoints.length > 0 ? (
-        convergencePoints.map(convergencePoint =>
-          convergencePoint.questions.map(question => (
-            <Box margin={10}>
-              <strong key={question.id}>{question.text}</strong>
-              <UnorderedList margin={5}>
-                {question.options.map(option => (
-                  <ListItem key={option.id}>
-                    {option.text}: {(option.average * 100).toFixed(2)}%
-                  </ListItem>
-                ))}
-              </UnorderedList>
-            </Box>
-          ))
-        )
+        <>
+          <Box display='flex' justifyContent='flex-end'>
+            <CSVLink data={csv} filename='strateegia_convergence_points_report-csv.csv'>
+              <Button
+                size='xs'
+                fontSize='14px'
+                fontWeight='400'
+                bg='#6c757d' 
+                color='#fff'
+                borderRadius='3px'
+                _hover={{bg: '#5C636A'}}
+                paddingBottom={'4px'}
+              >
+                csv
+              </Button>
+            <Button
+              m='2px'
+              size='xs'
+              fontSize='14px'
+              fontWeight='400'
+              bg='#6c757d' 
+              color='#fff'
+              borderRadius='3px'
+              _hover={{bg: '#5C636A'}}
+              paddingBottom={'4px'}
+              onClick={() => exportJSONData(csv)}
+            >
+              json
+            </Button>
+            </CSVLink>
+          </Box>
+          {convergencePoints.map(convergencePoint =>
+            convergencePoint.questions.map(question => (
+              <Box margin={10}>
+                <strong key={question.id}>{question.text}</strong>
+                <UnorderedList margin={5}>
+                  {question.options.map(option => (
+                    <ListItem key={option.id}>
+                      {option.text}: {(option.average * 100).toFixed(2)}%
+                    </ListItem>
+                  ))}
+                </UnorderedList>
+              </Box>
+            ))
+          )}
+
+        </>
       ) : (
         <p>sem pontos de convergência</p>
       )}
     </Box>
   );
+}
+
+export const exportJSONData = (data) => {
+  const jsonString = `data:text/json;chatset=utf-8,${encodeURIComponent(
+    JSON.stringify(data)
+  )}`;
+
+  const link = document.createElement("a");
+  link.href = jsonString;
+  link.download = "strateegia_convergence_points_report-json.json";
+
+  link.click();
 }
